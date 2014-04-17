925df9e54b03ece277f6997a7859d449acc02f45
szheng11 <shuzheng.zheng@globalonlineacademy.org>
2014-04-17 15:29:54 -0500

    initial comment

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Monster Nian
+//
+//  Created by Shuzheng Zheng on 4/17/14.
+//  Copyright (c) 2014 Shuzheng Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
